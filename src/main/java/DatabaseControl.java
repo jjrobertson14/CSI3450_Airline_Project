2ac4f4b083753ef68b49edcd5d537de426b914ed
Noah G <nagiles@oakland.edu>
2017-11-24 16:17:36 -0500

    Clicking the 'Populate Airports' button in the database tab now produces 4 demo values

--- src/main/java/DatabaseControl.java
+++ src/main/java/DatabaseControl.java
@@ -29,9 +29,33 @@ public class DatabaseControl extends GridPane {
         populateEmployeeButton = new Button("Populate Employees");
 
         // insert a group of random customers into the database
-        populateCustomerButton.setOnAction(e ->     {
+        populateCustomerButton.setOnAction( e -> {
             Customer[] c = Customer.generateRandomCustomers();
             executor.insertCustomer(c);
+        });
+
+        populateAirportButton.setOnAction( e -> {
+            Airport detroit = new Airport();
+            detroit.setName("Detroit Metroplitan Airport");
+            detroit.setLatitude(42.22);
+            detroit.setLongitude(83.36);
+
+            Airport chicago = new Airport();
+            chicago.setName("O'Hare International Airport");
+            chicago.setLatitude(41.97);
+            chicago.setLongitude(87.91);
+
+            Airport dallas = new Airport();
+            dallas.setName("Fort Worth International Airport");
+            dallas.setLatitude(32.90);
+            dallas.setLongitude(97.04);
+
+            Airport london = new Airport();
+            london.setName("Heathrow Airport");
+            london.setLatitude(51.47);
+            london.setLongitude(0.45);
+
+            executor.insertAirport(detroit, chicago, dallas, london);
         });
 
         // position buttons within the gridPane
@@ -40,10 +64,6 @@ public class DatabaseControl extends GridPane {
         this.add(populateAircraftButton, 1, 2);
         this.add(populateFlightButton, 1, 3);
         this.add(populateEmployeeButton, 1, 4);
-    }
-
-    private void populateCustomers () {
-        System.out.println("Populate customers!");
     }
 
 }
