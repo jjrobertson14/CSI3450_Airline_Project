583eddf36a53c2122752125bba94207945c1785e
Noah G <nagiles@oakland.edu>
2017-11-30 16:27:08 -0500

    Add employee data type

---
+++ src/main/java/Employee.java
@@ -0,0 +1,66 @@
+
+public class Employee {
+
+	private int id;
+	
+	private int prevFlightID;
+	
+	private int positionID;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	public Employee() {
+		
+	}
+
+	public Employee(int id, int prevFlightID, int positionID, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.prevFlightID = prevFlightID;
+		this.positionID = positionID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getPrevFlightID() {
+		return prevFlightID;
+	}
+
+	public void setPrevFlightID(int prevFlightID) {
+		this.prevFlightID = prevFlightID;
+	}
+
+	public int getPositionID() {
+		return positionID;
+	}
+
+	public void setPositionID(int positionID) {
+		this.positionID = positionID;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
