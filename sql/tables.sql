5d9c9e1ffadd59dd21a102352ceddf8d0fe075a8
Noah G <nagiles@oakland.edu>
2017-12-02 14:59:41 -0500

    Minor table alterations

--- sql/tables.sql
+++ sql/tables.sql
@@ -56,12 +56,13 @@ CREATE TABLE Reservation(
 );
 
 CREATE TABLE Purchase(
-    reservationID int not null primary key,
+    reservationID int not null,
     customerID int not null,
     paymentMethod enum ('debit', 'credit'),
     paymentDate date,
     foreign key (reservationID) references Reservation(reservationID),
-    foreign key (customerID) references Customer(customerID)
+    foreign key (customerID) references Customer(customerID),
+    primary key (reservationID, customerID)
 );
 
 CREATE TABLE Charge(
@@ -73,6 +74,7 @@ CREATE TABLE Charge(
     refund decimal,
     weightFee decimal,
     insuranceFee decimal,
+    cancellationFee decimal,
     ticketPrice decimal not null,
     foreign key (reservationID) references Reservation(reservationID),
     foreign key (customerID) references Customer(customerID),
